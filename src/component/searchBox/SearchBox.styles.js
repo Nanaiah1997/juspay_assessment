3790f1033f907c3e17d518f3f61e8fea96ec209c
Nanaiah.G <Nanaiah.g@analyttica.com>
2024-10-13 14:07:24 +0530

    change

--- src/component/searchBox/SearchBox.styles.js
+++ src/component/searchBox/SearchBox.styles.js
@@ -1,21 +1,18 @@
 import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles((theme) => {
-  console.log(theme, ":::theme"); // Check the theme structure
-  return {
-    searchBox: {
-      padding: "0.47rem 1.00rem",
-      borderRadius: "6px",
-      border: `.1rem solid ${theme.palette.text.primary}`,
-      "& svg": {
-        width: "1.26rem",
-        height: "1.26rem",
-      },
-      "& .MuiInputBase-input": {
-        fontSize: "1.11rem",
-      },
+const useStyles = makeStyles((theme) => ({
+  searchBox: {
+    padding: "0.47rem 1.00rem",
+    borderRadius: "6px",
+    border: `.1rem solid ${theme.palette.text.primary}`,
+    "& svg": {
+      width: "1.26rem",
+      height: "1.26rem",
     },
-  };
-});
+    "& .MuiInputBase-input": {
+      fontSize: "1.11rem",
+    },
+  },
+}));
 
 export default useStyles;
